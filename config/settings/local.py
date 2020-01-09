0fdd22b7bceffb5ee9b408866744f5799c68fd58
Daniel Stiner <danstiner@gmail.com>
2020-01-08 22:25:15 -0800

    Add django.contrib.gis  to installed apps

--- config/settings/local.py
+++ config/settings/local.py
@@ -62,3 +62,4 @@ INSTALLED_APPS += ["django_extensions"]  # noqa F405
 
 # Your stuff...
 # ------------------------------------------------------------------------------
+INSTALLED_APPS += ["django.contrib.gis"]  # noqa F405
